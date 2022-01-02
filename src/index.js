8605c60f8a978ebd0c5cddc6da25ed229853612c
Giuseppe Barillari <joe2k01dev@gmail.com>
2022-01-02 16:06:58 +0000

    Generate 9x9 table
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,17 +1,90 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Box(props) {
+    return (
+        <input type="number" min="0" max="9" value={props.value}/>
+    );
+}
+
+class SudokuBoard extends React.Component {
+    renderBox(i) {
+        return(
+            <Box
+                value={this.props.boxes[i]}
+            />
+        )
+    }
+
+    render() {
+        const mBoxes = Array(81).fill(null);
+        for(let i = 0; i < 81; i++) {
+            mBoxes[i] = this.renderBox(i);
+            console.log(i);
+        }
+
+        const mLines = Array(9).fill(null);
+        const line = Array(9).fill(null);
+        let linesCount = 0;
+        let lineCount = 0;
+        // Generate 9x9 array
+        for(let j = 0; j < 81; j++) {
+            line[lineCount] = mBoxes[j];
+            lineCount++;
+            if(lineCount === 9) {
+                mLines[linesCount] = line;
+                linesCount++;
+                lineCount = 0;
+            }
+        }
+
+        return (
+            <div>
+                <table>
+                    <tbody>
+                    {
+                        mLines.map(row =>
+                            <tr>
+                                {
+                                    row.map(box =>
+                                        <td>{box}</td>
+                                    )
+                                }
+                            </tr>
+                        )
+                    }
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+
+class Sudoku extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            // Sudoku has 81 boxes
+            boxes: Array(81).fill(""),
+        }
+    }
+
+    render() {
+        return (
+            <div className="sudou">
+                <div className="sudoku-board">
+                    <SudokuBoard
+                        boxes={this.state.boxes}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Sudoku />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

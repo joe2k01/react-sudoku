cdf2c7ee4f536fbd5bb6f2bb9d9c1be613a94d76
Giuseppe Barillari <joe2k01dev@gmail.com>
2022-01-03 04:38:17 +0000

    Convert to using buttons
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,60 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+//================================
+let ids = 0;
+const ID = () => ++ids;
+//================================
+
 function Box(props) {
     return (
-        <input type="number" min="0" max="9" value={props.value}/>
+        <button className="box" onClick={props.onClick}>
+            {props.value}
+        </button>
     );
 }
 
 class SudokuBoard extends React.Component {
-    renderBox(i) {
-        return(
-            <Box
-                value={this.props.boxes[i]}
-            />
-        )
-    }
-
     render() {
-        const mBoxes = Array(81).fill(null);
-        for(let i = 0; i < 81; i++) {
-            mBoxes[i] = this.renderBox(i);
-            console.log(i);
-        }
-
-        const mLines = Array(9).fill(null);
-        const line = Array(9).fill(null);
-        let linesCount = 0;
-        let lineCount = 0;
-        // Generate 9x9 array
-        for(let j = 0; j < 81; j++) {
-            line[lineCount] = mBoxes[j];
-            lineCount++;
-            if(lineCount === 9) {
-                mLines[linesCount] = line;
-                linesCount++;
-                lineCount = 0;
-            }
-        }
-
+        let outer;
         return (
-            <div>
-                <table>
-                    <tbody>
-                    {
-                        mLines.map(row =>
-                            <tr>
-                                {
-                                    row.map(box =>
-                                        <td>{box}</td>
-                                    )
-                                }
-                            </tr>
-                        )
-                    }
-                    </tbody>
-                </table>
+            <div className="sudoku">
+                {
+                    this.props.boxes.map((row, i) => 
+                        <div className="row" key={ID()}>
+                            {
+                                row.map((val, j) => 
+                                <Box
+                                    value={this.props.boxes[i][j]}
+                                    onChange={() => this.props.onChange(i)}
+                                    key={ID()}
+                                />
+                                )
+                            }
+                        </div>
+                    )
+                }
             </div>
         );
     }
@@ -67,8 +46,12 @@ class Sudoku extends React.Component {
         super(props);
         this.state = {
             // Sudoku has 81 boxes
-            boxes: Array(81).fill(""),
+            boxes: Array(9).fill(Array(9).fill("")),
         }
+    }
+
+    handleChange(i) {
+
     }
 
     render() {
@@ -77,6 +60,7 @@ class Sudoku extends React.Component {
                 <div className="sudoku-board">
                     <SudokuBoard
                         boxes={this.state.boxes}
+                        onChange={(i) => this.handleChange(i)}
                     />
                 </div>
             </div>

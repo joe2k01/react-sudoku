690b5b2b275e5b69f3847f52b9ef8699e8b5f15c
Giuseppe Barillari <joe2k01dev@gmail.com>
2022-01-10 15:03:19 +0100

    Give symmetric clues and set text red when input is wrong
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- src/index.js
+++ src/index.js
@@ -63,6 +63,10 @@ class SudokuBoard extends React.Component {
                                         }
                                     }
 
+                                    if(this.props.state.boxes[i][j] != "" && this.props.state.boxes[i][j] != this.props.state.solution[i][j]) {
+                                        computedClass = computedClass.concat(" wrong");
+                                    }
+
                                     return(
                                         <td className={`box ${computedClass}`} key={`${i}${j}`} onClick={() => this.props.boxClick(i, j)}>
                                             {this.props.state.boxes[i][j]}
@@ -97,7 +101,8 @@ class Sudoku extends React.Component {
         this.state = {
             // Sudoku has 81 boxes, arrays are funny in this language
             boxes: Array(9).fill().map(row => new Array(9).fill("")),
-            solution: null,
+            solution: Array(9).fill().map(row => new Array(9).fill("")),
+            prohibitedIndexes: Array(9).fill().map(row => new Array(9).fill("")),
             selectedOuter: null,
             selectedInner: null,
             numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9],
@@ -123,7 +128,7 @@ class Sudoku extends React.Component {
     }
 
     populateSudoku() {
-        var solution = Array(9).fill().map(row => new Array(9).fill(""));
+        let solution = this.state.solution;
         const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
                 
         // Shuffle array (Fisher-Yates algorithm)
@@ -149,11 +154,28 @@ class Sudoku extends React.Component {
         // Assign solution
         this.state.solution = solution;
 
-        // Give clues
-        for(var k = 0; k < 9; k++) {
+        // Give symmetric clues
+        let indexes = [];
+        for(var k = 0; k < 5; k++) {
+            let usedIndex;
             for(var l = 0; l < 2; l++) {
                 let randomIndex = Math.trunc(Math.random() * 9);
+                while(randomIndex === usedIndex) {
+                    randomIndex = Math.trunc(Math.random() * 9);
+                }
                 this.state.boxes[k][randomIndex] = solution[k][randomIndex];
+                indexes.push(randomIndex);
+                usedIndex = randomIndex;
+                console.log(`randomIndex: ${randomIndex}`);
+            }
+        }
+        let currentSymmIndex = 0;
+        for(var m = 8; m > 4; m--) {
+            for(var n = 0; n < 2; n++) {
+                let index = 8 - indexes[currentSymmIndex];
+                this.state.boxes[m][index] = solution[m][index];
+                console.log(`index: ${index} n: ${n}`);
+                currentSymmIndex++;
             }
         }
     }
